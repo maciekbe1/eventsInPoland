2c3d14698d9d44961adf89865d1d6959ef47737b
Piotr Frukacz <pfrukacz@gmail.com>
2019-05-29 11:01:01 +0200

    Add Events page with details, add contact page

--- client/src/components/EventsPage/Squqre.js
+++ client/src/components/EventsPage/Squqre.js
@@ -1,23 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import parse from "html-react-parser";
 
 export default function Squqre(props) {
     return (
-        <>
-            <Link
-                className="col-md-4 col-sm-6 event-block"
-                key={props.index}
-                to={`/all-events/event/${props.id}`}
-            >
-                <img
-                    src={`http://via.placeholder.com/450x300?text=${
-                        props.title
-                    }`}
-                    alt=""
-                />
-                <p>{props.title}</p>
-                <p>{props.date}</p>
-            </Link>
-        </>
+        <Link
+            className="col-md-4 col-sm-6 event-block"
+            key={props.index}
+            to={`/all-events/event/${props.id}`}
+        >
+            {props.img.map((img, index) => {
+                if (img.text_value.indexOf("<img") === 0) {
+                    const image = parse(img.text_value);
+                    return <div key={index}>{image}</div>;
+                }
+
+                return null;
+            })}
+
+            <p>{props.title}</p>
+            <p>{props.date}</p>
+        </Link>
     );
 }
